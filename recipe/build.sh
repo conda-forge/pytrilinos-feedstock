a736abcdc6e2c99ec299d9945262428229bea6ba
Bill Spotz <wfspotz@sandia.gov>
2018-07-18 15:53:54 -0600

    Update pytrilinos-12.10.1 package to depend on trilinos-12.10.1
    Issues addressed:
    
        * The build pulls in headers, and more specifically, configure
          headers, from the system environment, meaning that the
          trilinos-12.10.1 headers are getting pulled in, not the
          pytrilinos-12.10.1 headers. For EpetraExt, this means that
          HAVE_PYTRILINOS is not set, which means the friend functions
          convertInArgsFromPython() and convertOutArgsFromPython() are not
          defined. I fix this with a patch, in whcih PyTrilinos_config.h
          sets HAVE_PYTRILINOS, and add it to 12.10-fixes.patch.
    
        * Since the trilinos-12.10.1 headers are loaded, I can't use a
          reduced set of headers for pytrilinos-12.10.1. The build.sh
          script reflects this, and is altered to match the
          trilinos-12.10.1 build.sh in other ways as well. Per my
          discussion with @isuruf, I do not use the configFix.py script.
    
        * The big change in meta.yaml is the dependency on
          trilinos-12.10.1, plus some other things to make it more like
          the trilinos-12.10.1 meta.yaml.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,22 +1,15 @@
 mkdir -p build
 cd build
 
-REMOVE="-Wl,-rpath,${PREFIX}/lib"
-export LDFLAGS=${LDFLAGS//$REMOVE}
-
 if [ $(uname) == Darwin ]; then
     export CXXFLAGS="$CXXFLAGS -stdlib=libc++"
 fi
 
-# export MPI_FLAGS="--allow-run-as-root"
-# if [ $(uname) == Linux ]; then
-#     export MPI_FLAGS="$MPI_FLAGS;-mca;plm;isolated"
-# fi
+export MPI_FLAGS="--allow-run-as-root"
 
-export HYDRA_LAUNCHER=fork
-export OMPI_MCA_plm=isolated
-export OMPI_MCA_btl_vader_single_copy_mechanism=none
-export OMPI_MCA_rmaps_base_oversubscribe=yes
+if [ $(uname) == Linux ]; then
+    export MPI_FLAGS="$MPI_FLAGS;-mca;plm;isolated"
+fi
 
 cmake \
   -D CMAKE_BUILD_TYPE:STRING=RELEASE \
@@ -35,26 +28,53 @@ cmake \
   -D Trilinos_ENABLE_TESTS:BOOL=OFF \
   -D Trilinos_ENABLE_EXAMPLES:BOOL=OFF \
   -D Trilinos_ENABLE_Teuchos:BOOL=ON \
+  -D Trilinos_ENABLE_RTOp:BOOL=ON \
+  -D Trilinos_ENABLE_Sacado:BOOL=ON \
+  -D Trilinos_ENABLE_MiniTensor:BOOL=ON \
   -D Trilinos_ENABLE_Epetra:BOOL=ON \
+  -D Trilinos_ENABLE_Zoltan:BOOL=ON \
+  -D Trilinos_ENABLE_Shards:BOOL=ON \
+  -D Trilinos_ENABLE_GlobiPack:BOOL=ON \
   -D Trilinos_ENABLE_Triutils:BOOL=ON \
   -D Trilinos_ENABLE_Tpetra:BOOL=ON \
   -D Trilinos_ENABLE_EpetraExt:BOOL=ON \
   -D Trilinos_ENABLE_Domi:BOOL=ON \
+  -D Trilinos_ENABLE_Thyra:BOOL=ON \
+  -D Trilinos_ENABLE_Xpetra:BOOL=ON \
+  -D Trilinos_ENABLE_OptiPack:BOOL=ON \
   -D Trilinos_ENABLE_Isorropia:BOOL=ON \
   -D Trilinos_ENABLE_Pliris:BOOL=ON \
   -D Trilinos_ENABLE_AztecOO:BOOL=ON \
   -D Trilinos_ENABLE_Galeri:BOOL=ON \
   -D Trilinos_ENABLE_Amesos:BOOL=ON \
+  -D Trilinos_ENABLE_Pamgen:BOOL=ON \
+  -D Trilinos_ENABLE_Zoltan2:BOOL=ON \
   -D Trilinos_ENABLE_Ifpack:BOOL=ON \
   -D Trilinos_ENABLE_ML:BOOL=ON \
+  -D Trilinos_ENABLE_Belos:BOOL=ON \
+  -D Trilinos_ENABLE_ShyLU:BOOL=ON \
+  -D Trilinos_ENABLE_Amesos2:BOOL=ON \
+  -D Trilinos_ENABLE_SEACAS:BOOL=OFF \
   -D Trilinos_ENABLE_Komplex:BOOL=ON \
   -D Trilinos_ENABLE_Anasazi:BOOL=ON \
+  -D Trilinos_ENABLE_Ifpack2:BOOL=ON \
+  -D Ifpack2_ENABLE_TESTS:BOOL=OFF \
+  -D Trilinos_ENABLE_Stratimikos:BOOL=ON \
+  -D Trilinos_ENABLE_FEI:BOOL=ON \
+  -D Trilinos_ENABLE_Teko:BOOL=ON \
+  -D Trilinos_ENABLE_Intrepid:BOOL=ON \
+  -D Trilinos_ENABLE_STK:BOOL=OFF \
+  -D Trilinos_ENABLE_Phalanx:BOOL=ON \
   -D Trilinos_ENABLE_NOX:BOOL=ON \
   -D NOX_ENABLE_LOCA:BOOL=ON \
+  -D Trilinos_ENABLE_MueLu:BOOL=ON \
+  -D Trilinos_ENABLE_Rythmos:BOOL=ON \
+  -D Trilinos_ENABLE_Stokhos:BOOL=ON \
+  -D Trilinos_ENABLE_ROL:BOOL=ON \
+  -D Trilinos_ENABLE_Piro:BOOL=ON \
+  -D Trilinos_ENABLE_TrilinosCouplings:BOOL=ON \
+  -D Trilinos_ENABLE_Pike:BOOL=ON \
   -D Trilinos_ENABLE_PyTrilinos:BOOL=ON \
   $SRC_DIR
 
-make -j $CPU_COUNT
-$PYTHON packages/PyTrilinos/util/configFix.py
-ctest --output-on-failure
-make install
+make -j $CPU_COUNT install
